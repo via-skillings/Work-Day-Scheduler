be869c7b90470e3492b09a722ac3f817a9ec2d9c
Via Skillings <viaskillings@gmail.com>
2024-05-18 12:58:08 -0500

    fixed transparence issue

--- script.js
+++ script.js
@@ -1,55 +1,52 @@
-$(document).ready(function() {//loads 1)html 2)css first.
-  //display current time & date.
+$(document).ready(function() {
+  // Display current time & date.
   var currentDayElement = $('#currentDay');
   currentDayElement.text(dayjs().format('dddd, MMMM D'));
-});
 
- //click listener from users input on time stamp.
- $(".saveBtn").on("click", function () {
-  //get nearby values.
-  console.log(this);
-  var text = $(this).siblings(".description").val();
-  var time = $(this).parent().attr("id");
+  // Click listener for user's input on time stamp.
+  $(".saveBtn").on("click", function () {
+    // Get nearby values.
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
 
-  //loads local storage
-  localStorage.setItem(time, text);
-})
+    // Save to local storage.
+    localStorage.setItem(time, text);
+  });
 
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
+  // Load saved data from local storage.
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
 
-function hourTracker() {
-  //get current number of hours.
-  var currentHour = moment().hour();
+  function hourTracker() {
+    // Get current number of hours.
+    var currentHour = dayjs().hour();
 
-  // loop over time blocks
-  $(".time-block").each(function () {
+    // Loop over time blocks.
+    $(".time-block").each(function () {
       var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-      console.log( blockHour, currentHour)
+      console.log(blockHour, currentHour);
 
-      //check if time is past, present or future.
+      // Check if time is past, present or future.
       if (blockHour < currentHour) {
-          $(this).addClass("past");
-          $(this).removeClass("future");
-          $(this).removeClass("present");
+        $(this).addClass("past");
+        $(this).removeClass("future present");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past present");
+        $(this).addClass("future");
       }
-      else if (blockHour === currentHour) {
-          $(this).removeClass("past");
-          $(this).addClass("present");
-          $(this).removeClass("future");
-      }
-      else {
-          $(this).removeClass("present");
-          $(this).removeClass("past");
-          $(this).addClass("future");
-      }
-  })
-}
-hourTracker();
+    });
+  }
+
+  // Run hourTracker to update the classes.
+  hourTracker();
+});
